2a92e7e29c1df14da3b532b669d4f06c2c5c2fa7
bitterfly <dageneva@gmail.com>
2017-01-27 16:37:12 +0200

    Add some more models and fix indentation

--- src/bikesurf/lib/bikesurf/database/models.rb
+++ src/bikesurf/lib/bikesurf/database/models.rb
@@ -15,26 +15,54 @@ module Bikesurf
       property :desctiption,               Text
       property :frame,                     Integer
       property :crossbar,                  Integer
+      property :size,                      String
       property :front_lights,              String
       property :back_lights,               String
       property :backpedal_breaking_system, Boolean
       property :quick_release_saddle,      Boolean
       property :gears_number,              Integer
+      property :min_borrow_days,           Integer
+      property :max_borrow_days,           Integer
+
+      belongs_to :stand
+      has n, :reservations
     end
 
     class User
       include DataMapper::Resource
 
-      property   :id,       Serial
-      property   :username, String
-      property   :password, BCryptHash
-      property   :name,     String
-      property   :email,    String
+      property   :id,        Serial
+      property   :username,  String
+      property   :password,  BCryptHash
+      property   :name,      String
+      property   :email,     String
       property   :verified,  Boolean
      
       belongs_to :role
+      has n, :reservations
     end
 
+    class Stand
+      include DataMapper::Resource
+
+      property :id,       Serial
+      property :location, String
+
+      belongs_to :user
+    end
+
+    class Reservation
+      include DataMapper::Resource
+
+      property :id,            Serial
+      property :until,         Date
+      property :from,          Date
+      property :pick_up_time,  DateTime
+
+      belongs_to :bike
+      belongs_to :user
+    end
+    
     class Role
       include DataMapper::Resource
       property :id,         Serial

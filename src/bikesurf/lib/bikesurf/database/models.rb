552979cfce1570941f18d9f40b6ed4efaac5eab2
bitterfly <dageneva@gmail.com>
2017-01-27 14:55:10 +0200

    Add roles and permissions tables

--- src/bikesurf/lib/bikesurf/database/models.rb
+++ src/bikesurf/lib/bikesurf/database/models.rb
@@ -22,6 +22,47 @@ module Bikesurf
       property :gears_number,              Integer
     end
 
+    class User
+      include DataMapper::Resource
+
+      property   :id,       Serial
+      property   :username, String
+      property   :password, BCryptHash
+      property   :name,     String
+      property   :email,    String
+      property   :verified,  Boolean
+     
+      belongs_to :role
+    end
+
+    class Role
+      include DataMapper::Resource
+      property :id,         Serial
+      property :name,       String
+
+      has n, :users
+      has n, :role_permissions
+      has n, :permissions,  :through => :role_permissions
+    end
+
+    class Permission
+      include DataMapper::Resource
+      property :id,     Serial
+      property :name,   String
+
+      has n, :role_permissions
+      has n, :roles,    :through => :role_permissions
+    end
+
+    class RolePermission
+      include DataMapper::Resource
+
+      property :id,         Serial
+
+      belongs_to :role
+      belongs_to :permission
+    end
+
     DataMapper.finalize
   end
 end

5728fd4cdb99dbe3fb17443721d6af2843071c85
Georgi Pavlov <georgipavlov94@gmail.com>
2017-01-29 22:47:51 +0200

    Add example javascript 'class'

---
+++ src/bikesurf/public/models/user.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    App.User = function(params) {
+        this.id = params.id;
+        this.username = params.username;
+        this.name = params.name;
+    };
+
+})();

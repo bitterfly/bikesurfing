9aa6bfe18a28dd01d07f946b4ab3b34ef54ecac4
Georgi Pavlov <georgipavlov94@gmail.com>
2017-02-02 21:36:28 +0200

    Add component manager to sammy routes

--- src/bikesurf/public/index.js
+++ src/bikesurf/public/index.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+
     Sammy(function() {
-        this.get("#/", function() {});
+
+        var bikesurfViewModel = new App.BikesurfViewModel();
+        var componentManager = new App.ComponentManager(bikesurfViewModel);
+        bikesurfViewModel.selectedComponent = componentManager.selectedComponent;
+
+        ko.applyBindings(bikesurfViewModel);
+
+
+        this.get('/#/bike/:id', function() {
+            componentManager.selectComponent('bike_page', {id: this.params['id']})
+        });
 
         this.get('', function() {
-            this.redirect('#/');
+            this.redirect('/#/bike/4');
         });
     }).run();
 });

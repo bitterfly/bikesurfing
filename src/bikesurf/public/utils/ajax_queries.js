5b7f9854caaa8adaa393199a00cc2a19cf662724
Angel Angelov <hextwoa@gmail.com>
2017-02-02 18:11:22 +0200

    fix error handling - server errors are now correctly displayed

--- src/bikesurf/public/utils/ajax_queries.js
+++ src/bikesurf/public/utils/ajax_queries.js
@@ -1,61 +1,66 @@
-App.error = function(error) {
-    var message;
-    if (typeof(error) == 'string') {
-        message = error;
-    } else {
-        message = error.message;
+(function() {
+    App.error = function(error) {
+        var message;
+        if (typeof(error) == 'string') {
+            message = error;
+        } else {
+            message = error.message;
+        }
+
+        alert('Error: ' + message);
     }
 
-    alert('Error: ' + message);
-}
+    App.request = function(url, data, success, error_callback) {
+        var throw_error = function(error) {
+            if (error_callback) {
+                error_callback(error)
+            } else {
+                App.error(error);
+            }
+        };
 
-App.request = function(url, data, success, error_callback) {
-    $.ajax({
-        url: url,
-        data: JSON.stringify({
-            data: data,
-            session_id: 'dummy',
-        }),
-        method: 'POST',
-        success: function(response) {
-            response = JSON.parse(response);
+        var inspect_response = function(response_text) {
+            response = JSON.parse(response_text);
             if (!response) {
-                error = {
+                throw_error({
                     type: 'invalid_response',
-                    message: 'Error: server returned invalid JSON'
-                };
-                if (error_callback) {
-                    error_callback(error)
-                } else {
-                    App.error(error);
-                }
+                    text: response_text,
+                    message: 'Error: server returned invalid JSON: ' + response_text
+                });
                 return;
             }
 
             if (!response.ok) {
-                console.log(response);
-                console.log(response.ok);
-                error = {
+                throw_error({
                     type: 'server_error',
                     data: response.data,
                     message: 'Server error: ' + response.data
-                };
-                if (error_callback) {
-                    error_callback(error)
-                } else {
-                    App.error(error);
-                }
+                });
             } else {
                 success(response.data);
             }
-        },
-        error:  function(xhr, error, errorText) {
-            App.error({
-                type: 'http_error',
-                data: error,
-                text: errorText,
-                message: 'HTTP error ' + error + ': ' + errorText
-            });
-        }
-    });
-}
+        };
+
+        $.ajax({
+            url: url,
+            data: JSON.stringify({
+                data: data,
+                session_id: 'dummy',
+            }),
+            method: 'POST',
+            success: inspect_response,
+            error:  function(xhr, error, errorText) {
+                if (!xhr.responseText) {
+                    throw_error({
+                        type: 'http_error',
+                        data: error,
+                        text: errorText,
+                        message: 'HTTP error ' + error + ': ' + errorText
+                    });
+                } else {
+                    inspect_response(xhr.responseText);
+                }
+            }
+        });
+    }
+})();

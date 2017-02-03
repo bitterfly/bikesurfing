a6fb6a2e3cce28f160054fefa3fc88fb5fa348e2
Angel Angelov <hextwoa@gmail.com>
2017-02-03 21:01:07 +0200

    bike page shows bike photos and comments

--- src/bikesurf/public/view_models/bike_page.js
+++ src/bikesurf/public/view_models/bike_page.js
@@ -2,9 +2,22 @@
     App.BikePageViewModel = function(params) {
         this.id = ko.observable(params['id']);
         this.bike_info = ko.observable();
+        this.bike_images = ko.observable();
+        this.comments = ko.observable();
+
         ko.computed(function() {
             this.bike_info(undefined);
             App.request('bike', { bike_id: this.id() }, this.bike_info);
+        }, this);
+
+        ko.computed(function() {
+            this.bike_images(undefined);
+            App.request('images/bike', { bike_id: this.id() }, this.bike_images);
+        }, this);
+
+        ko.computed(function() {
+            this.comments(undefined);
+            App.request('comments/bike', { bike_id: this.id() }, this.comments);
         }, this);
 
         this.bike = ko.pureComputed(function() {
@@ -22,39 +35,18 @@
         this.bike_image_index = ko.observable(0);
 
         this.bike_image_link = ko.pureComputed(function() {
-            if (this.bike_info()) {
-                return App.image_url(this.bike_info().images[this.bike_image_index()]);
+            if (this.bike_images()) {
+                return App.image_url(this.bike_images()[this.bike_image_index()]);
             } else {
                 return null;
             }
         }, this);
 
         this.next_image = function() {
-            var num_images = this.bike_info().images.length;
-            console.log(this.bike_info().images);
+            var num_images = this.bike_images().length;
             this.bike_image_index((this.bike_image_index() + 1) % num_images);
         };
 
 
-        this.comments = ko.observable([
-            {
-                username: 'Devourer',
-                user_avatar: 'https://www.heroesofnewerth.com/images/heroes/6/icon_128.jpg',
-                timestamp: '2017-02-01 00:52',
-                content: "You're mine!"
-            },
-            {
-                username: 'Emerald Warden',
-                user_avatar: 'https://www.heroesofnewerth.com/images/heroes/195/icon_128.jpg',
-                timestamp: '2017-02-01 00:58',
-                content: "Quite astounding speed! Would recommend 10/10."
-            },
-            {
-                username: 'Engineer',
-                user_avatar: 'https://www.heroesofnewerth.com/images/heroes/122/icon_128.jpg',
-                timestamp: '2017-02-02 13:12',
-                content: "Gaaaaaaaaaaaaaay"
-            }
-        ]);
     }
 })();

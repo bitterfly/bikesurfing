98417d7469d77a30c041cdad59406eb332dfb191
Georgi Pavlov <georgipavlov94@gmail.com>
2017-02-03 19:37:58 +0200

    Add menu button functionality

--- src/bikesurf/public/view_models/bikesurf.js
+++ src/bikesurf/public/view_models/bikesurf.js
@@ -4,6 +4,16 @@
     App.BikesurfViewModel = function() {
         var self = this;
         self.asd = ko.observable("asd");
+
+        self.openMenu = function() {
+            $('.sidenav').css({'width': '250px'})
+            $('#page').css({'margin-left': '250px'})
+        }
+
+        self.closeMenu = function() {
+            $('.sidenav').css({'width': '0'})
+            $('#page').css({'margin-left': '0'})
+        };
     }
 
 })();

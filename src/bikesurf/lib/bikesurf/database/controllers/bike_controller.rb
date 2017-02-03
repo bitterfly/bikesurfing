3f4354839d821c0255b6c20f92b21d7657998f5a
bitterfly <dageneva@gmail.com>
2017-02-03 19:39:50 +0200

    Returning comments

--- src/bikesurf/lib/bikesurf/database/controllers/bike_controller.rb
+++ src/bikesurf/lib/bikesurf/database/controllers/bike_controller.rb
@@ -9,16 +9,29 @@ module Bikesurf
     class BikeController
       include Singleton
       def get_by_id(id)
-        #image_ids = Models::BikeImage.all(bike_id: id, :only=>[:image_id])
-        image_ids = [1, 2, 3]
-        image_files = Models::Image.all(id: image_ids, fields: [:filename])
-
-        puts(image_ids)
         {
           bike: Models::Bike.get!(id),
           reservations: Models::Reservation.all(bike_id: id),
-          images: Models::Image.all(bike_image: Models::BikeImage.all(bike_id: id))
         }
+      end
+
+      def get_images_by_id(id)
+        {
+          images: Models::Image.all(bike_image: Models::BikeImage.first(bike_id: id))
+        }
+      end
+
+      def get_comments_by_id(id)
+        comments = Models::Comment.all(bike_comment: Models::BikeComment.first(bike_id: id))
+        
+        fyck = []
+        comments.each do |comment|
+         fyck << {
+            comment: comment,
+            user: comment.user
+          }
+        end
+        {comments: fyck}
       end
 
       def get_all

542f75a3cd9737b9f570e37328f9d16bddf92841
Zvezdi Dimitrova <zvezdi.dim@gmail.com>
2017-02-04 12:01:59 +0200

    WIP

--- src/bikesurf/lib/bikesurf/requests/search.rb
+++ src/bikesurf/lib/bikesurf/requests/search.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
 require 'bikesurf/database/controllers'
+require 'bikesurf/helpers/date_helper'
 
 module Bikesurf
   module Requests
     module Search
       def bike_search(from, to, size)
+        from = date_of(from)
+        to = date_of(to)
+
         reserved_ids = Database::ReservationController.instance.get_reserved_bike_ids(from, to)
         Database::BikeController.instance.free_bikes(reserved_ids, size)
       end

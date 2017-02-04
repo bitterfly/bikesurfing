f512ccca87299080d0b29a3ed4ae79701a04290d
Zvezdi Dimitrova <zvezdi.dim@gmail.com>
2017-02-04 17:25:14 +0200

    Methods in models should be class methods, oops

--- src/bikesurf/lib/bikesurf/helpers/date_helper.rb
+++ src/bikesurf/lib/bikesurf/helpers/date_helper.rb
@@ -1,11 +1,11 @@
 require 'date'
 
 module DateHelper
-  def date_of(timestamp)
+  def self.date_of(timestamp)
     Time.at(timestamp).utc.to_date
   end
 
-  def timestamp_of(date)
+  def self.timestamp_of(date)
     date.to_time.utc.to_i
   end
 end

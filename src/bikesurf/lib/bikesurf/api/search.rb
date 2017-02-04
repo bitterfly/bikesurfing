e807ae1dd03aaceaf24b432879ef619c8ad6ad3b
Zvezdi Dimitrova <zvezdi.dim@gmail.com>
2017-02-04 18:59:49 +0200

    Change the way we use the DateHelper

--- src/bikesurf/lib/bikesurf/api/search.rb
+++ src/bikesurf/lib/bikesurf/api/search.rb
@@ -5,6 +5,7 @@ require 'bikesurf/helpers/date_helper'
 module Bikesurf
   module Requests
     module Search
+      include ::Bikesurf::Helpers::DateHelper
       def bike_search(from, to, size)
         from = date_of(from)
         to = date_of(to)
@@ -14,8 +15,8 @@ module Bikesurf
       end
 
       def get_free_bikes(from_timestamp, to_timestamp, size)
-        from_sql_datetime = DateHelper.timestamp_to_date(from_timestamp)
-        to_sql_datetime = DateHelper.timestamp_to_date(to_timestamp)
+        from_sql_datetime = timestamp_to_date(from_timestamp)
+        to_sql_datetime = timestamp_to_date(to_timestamp)
         Database::ReservationController.instance.free_bikes(from_sql_datetime, to_sql_datetime, size)
       end
     end
--- src/bikesurf/lib/bikesurf/helpers/date_helper.rb
+++ src/bikesurf/lib/bikesurf/helpers/date_helper.rb
@@ -1,21 +1,21 @@
 require 'date'
 
 module Bikesurf
-  module Helpes
+  module Helpers
     module DateHelper
-      def self.timestamp_to_date(timestamp)
+      def timestamp_to_date(timestamp)
         Time.at(timestamp).utc.to_datetime.to_date
       end
 
-      def self.date_to_timestamp(date)
+      def date_to_timestamp(date)
         Time.utc(date.year, date.month, date.day).to_i
       end
 
-      def self.timestamp_to_time(timestamp)
+      def timestamp_to_time(timestamp)
         Time.at(timestamp).utc
       end
 
-      def self.time_to_timestamp(time)
+      def time_to_timestamp(time)
         time.to_i
       end
     end

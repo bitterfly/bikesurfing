12c64304965b09fc273463ff47662ef39cfb0465
bitterfly <dageneva@gmail.com>
2017-02-07 18:03:53 +0200

    Fix search bikes

--- src/bikesurf/lib/bikesurf/database/controllers/reservation_controller.rb
+++ src/bikesurf/lib/bikesurf/database/controllers/reservation_controller.rb
@@ -8,29 +8,18 @@ module Bikesurf
     class ReservationController
       include Singleton
 
-      def free_fitting_criteria_bikes(from, to, criteria)
-        {
-          bikes: bikes_fitting_criteria(criteria) - reserved_bikes(from, to)
-        }
-      end
-
-      private
-
       def reserved_bikes(from, to)
         Models::Bike.all(
           reservations: Models::Reservation.all(
             :from.lte => to,
-            :until.gte => from
+            :until.gte => from,
+            status: 'accepted'
           )
         )
       end
 
       def free_bikes(from, to)
         Models::Bike.all - reserved_bikes(from, to)
-      end
-
-      def bikes_fitting_criteria(criteria)
-        Models::Bike.all(criteria)
       end
     end
   end

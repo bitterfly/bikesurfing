cd52b9f83b3c5157746cc43b76e924df8d229278
bitterfly <dageneva@gmail.com>
2017-02-07 23:12:01 +0200

    Add delete image

--- src/bikesurf/lib/bikesurf/api/image.rb
+++ src/bikesurf/lib/bikesurf/api/image.rb
@@ -11,6 +11,16 @@ module Bikesurf
         database_image = Images.save_to_database(image)
         Database::BikeImageController.instance.add(bike_id, database_image)
       end
+
+      def delete_image(bike_id, image_id)
+        filename = Database::ImageController.instance.delete(image_id)
+
+        Config::IMAGE_SIZES.each do |size|
+            resized_file = [filename, size.to_s].join('_')
+            File.delete(File.join(ENV['IMAGES'], resized_file))
+        end
+        File.delete(File.join(ENV['IMAGES'], filename))
+      end
     end
   end
 end

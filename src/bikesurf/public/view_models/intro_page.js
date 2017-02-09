b61bb6a7b2cd7540a6906a4627354955b3993507
Georgi Pavlov <georgipavlov94@gmail.com>
2017-02-09 11:35:29 +0200

    Make submit button appear if form is filled

--- src/bikesurf/public/view_models/intro_page.js
+++ src/bikesurf/public/view_models/intro_page.js
@@ -6,7 +6,11 @@
 
         self.dateFrom = ko.observable();
         self.dateTo = ko.observable();
-        self.bikeSize = ko.observable();
+        self.bikeSize = ko.observable("all");
+
+        self.submitReady = function() {
+            return self.dateFrom() && self.dateTo();
+        };
 
         self.submit = function(form) {
             console.log(form);

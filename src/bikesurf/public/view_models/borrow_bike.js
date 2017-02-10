f6caa9768fdfca0cca1120f3ef7b56097fc6cf03
Angel Angelov <hextwoa@gmail.com>
2017-02-10 11:59:30 +0200

    add borrow bike date pickers

--- src/bikesurf/public/view_models/borrow_bike.js
+++ src/bikesurf/public/view_models/borrow_bike.js
@@ -3,7 +3,17 @@
 
     App.BorrowBikeViewModel = function() {
         var self = this;
-        self.asd = ko.observable("The Sunk'n Norwegian");
+
+        self.dateFrom = ko.observable();
+        self.dateTo = ko.observable();
+
+        self.datepickerActive = ko.observable(false);
+
+        self.submitReady = ko.computed(function() {
+            return App.isDate(self.dateFrom()) && App.isDate(self.dateTo()) && !self.datepickerActive();
+        }, this);
+
+        App.initDatePickers(self, "#dates", "#dateFrom", "#dateTo");
     }
 
 })();

1744d7e552acace45ef871dca85b53fac4e14037
Angel Angelov <hextwoa@gmail.com>
2017-02-10 20:47:34 +0200

    all info in reservation page

--- src/bikesurf/public/view_models/reservation_page.js
+++ src/bikesurf/public/view_models/reservation_page.js
@@ -8,12 +8,22 @@
 
         ko.computed(function() {
             App.request('reservation', { reservation_id: self.id() }, self.reservation_info);
-        }, this);
+        }, self);
 
         self.reservation_info.subscribe(function(r) {
             console.log(r);
         });
 
+        self.user = ko.pureComputed(function() {
+            var info = self.reservation_info();
+            if (info.reservor.id == App.me().id) {
+                return info.lender;
+            } else {
+                return info.reservor;
+            }
+        }, self);
+
+        self.avatar_url = App.avatar_url;
         self.image_url = App.image_url;
 
         self.pretty_date = App.pretty_date;

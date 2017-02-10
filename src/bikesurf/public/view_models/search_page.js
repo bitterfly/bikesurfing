407eeb87d4f60ac314c110e44bf653c0295939bf
Georgi Pavlov <georgipavlov94@gmail.com>
2017-02-10 17:51:06 +0200

    Add form functionality

--- src/bikesurf/public/view_models/search_page.js
+++ src/bikesurf/public/view_models/search_page.js
@@ -1,9 +1,42 @@
 (function() {
     "use strict";
 
-    App.SearchPageViewModel = function() {
+    App.SearchPageViewModel = function(params) {
         var self = this;
 
+        self.dateFrom = ko.observable(params.from);
+        self.dateTo = ko.observable(params.to);
+        self.bikeSize = ko.observable(params.size);
+        self.gears = ko.observable(params.gears);
+        self.frontLights = ko.observable(toBool(params.front_lights));
+        self.backLights = ko.observable(toBool(params.back_lights));
+        self.backpedalBrake = ko.observable(toBool(params.backpedal_brake));
+        self.quickReleaseSaddle = ko.observable(toBool(params.quick_release_saddle));
+
+        self.datepickerActive = ko.observable(false);
+
+        function toBool(val) {
+            return (val === 'true');
+        }
+
+        self.submitReady = ko.computed(function() {
+            return App.isDate(self.dateFrom()) && App.isDate(self.dateTo()) && !self.datepickerActive();
+        }, this);
+
+        self.submit = function() {
+            window.location.hash = '/search?from=' + self.dateFrom() +'&to=' + self.dateTo() + '&size=' + self.bikeSize() +
+                '&gears=' + self.gears() + '&front_lights=' + self.frontLights() + '&back_lights=' + self.backLights() +
+                '&backpedal_brake=' + self.backpedalBrake() + '&quick_release_saddle=' + self.quickReleaseSaddle();
+            App.request('search_bikes', {
+                from: 1486598400,
+                to: 1486684800,
+                size: 'small',
+            }, function(data) {
+                $('#gallery').html(JSON.stringify(data));
+            });
+        };
+
+        App.initDatePickers(self, "#dates", "#dateFrom", "#dateTo");
     };
 
 })();

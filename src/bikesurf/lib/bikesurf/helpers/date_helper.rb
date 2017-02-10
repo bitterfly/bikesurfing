e9ea00e59cd2ceeb132de055cadbefc459ba6b66
bitterfly <dageneva@gmail.com>
2017-02-10 20:14:07 +0200

    return if nil time or date

--- src/bikesurf/lib/bikesurf/helpers/date_helper.rb
+++ src/bikesurf/lib/bikesurf/helpers/date_helper.rb
@@ -4,22 +4,22 @@ module Bikesurf
   module Helpers
     module DateHelper
       def timestamp_to_date(timestamp)
-        nil unless timestamp
+        return nil unless timestamp
         Time.at(timestamp).utc.to_datetime.to_date
       end
 
       def date_to_timestamp(date)
-        nil unless date
+        return nil unless date
         Time.utc(date.year, date.month, date.day).to_i
       end
 
       def timestamp_to_time(timestamp)
-        nil unless timestamp
+        return nil unless timestamp
         Time.at(timestamp).utc
       end
 
       def time_to_timestamp(time)
-        nil unless time
+        return nil unless time
         time.to_time.to_i
       end
 
